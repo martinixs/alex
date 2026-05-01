6f4274c50983472eda9dfe2d58e4b0fe6d87bbdf
Твоё Имя <MKaygorodova@alfabank.ru>
2026-05-01 20:44:01 +0300

    Change wait time

--- src/test/java/com/person/CaseOpenApp.java
+++ src/test/java/com/person/CaseOpenApp.java
@@ -21,11 +21,11 @@ public class CaseOpenApp extends BaseTest {
                 ExpectedConditions.visibilityOfElementLocated(By.id("login"))
         ).sendKeys("admin");
 
-        Thread.sleep(HALF_SECOND); //
+        Thread.sleep(ONE_AND_HALF); //
 
         driver.findElement(By.id("password")).sendKeys("1234");
 
-        Thread.sleep(HALF_SECOND); // ← пауза 2 секунды
+        Thread.sleep(ONE_AND_HALF); // ← пауза 2 секунды
 
         driver.findElement(By.className("btn-login")).click();
 
@@ -33,26 +33,26 @@ public class CaseOpenApp extends BaseTest {
                 ExpectedConditions.visibilityOfElementLocated(
                         By.xpath("//div[@class='hero-title']")));
 
-        Thread.sleep(HALF_SECOND); //
+        Thread.sleep(ONE_AND_HALF); //
         driver.findElement(By.xpath("//div[@class='app-card']/div/button[contains(@onclick, 'Приложение ЕРУФР')]"))
                 .click();
 
-        Thread.sleep(HALF_SECOND); //
+        Thread.sleep(ONE_AND_HALF); //
         wait.until(
                 ExpectedConditions.visibilityOfElementLocated(
                         By.xpath("//button[@class='btn-open-app']")));
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//button[@class='panel-tab-btn' and contains(text(), 'Релизы')]"))
                 .click();
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//button[contains(text(), 'О приложении')]")).click();
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//button[@class='btn-open-app']")).click();
 
-        Thread.sleep(HALF_SECOND); //
+        Thread.sleep(ONE_AND_HALF); //
 
         wait.until(
                 ExpectedConditions.visibilityOfElementLocated(
@@ -63,7 +63,7 @@ public class CaseOpenApp extends BaseTest {
         js.executeScript("arguments[0].click();", radio);
 
 
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("addFL")).click();
 
@@ -72,46 +72,46 @@ public class CaseOpenApp extends BaseTest {
                         By.xpath("//div[@class='modal-fl-header']")));
 
         driver.findElement(By.id("verify-surname")).sendKeys("Иванов");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("verify-name")).sendKeys("Иван");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("verify-midname")).sendKeys("Платонович");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("verify-inn")).sendKeys("569205585045");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("verify-snils")).sendKeys("631-137-298 65");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//button[@class='btn-modal-verify']")).click();
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         wait.until(
                 ExpectedConditions.visibilityOfElementLocated(
                         By.xpath("//div[contains(text(),'Создание нового физического лица')]")));
 
         driver.findElement(By.xpath("//input[@value='male']")).click();
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("fl-dob")).sendKeys("30.05.1980");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
 
         driver.findElement(By.id("doc-number"))
                 .sendKeys("1");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("doc-date"))
                 .sendKeys("04.04.2024");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
 
         driver.findElement(By.id("doc-start-date"))
                 .sendKeys("04.05.2024");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//button[@class='btn-modal-save']"))
                 .click();
@@ -122,21 +122,21 @@ public class CaseOpenApp extends BaseTest {
                         By.xpath("//main[@class='person-main']")));
 
         driver.findElement(By.id("search-form")).click();
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         WebElement radio1 = driver.findElement(By.xpath("//input[@value='fl']"));
         JavascriptExecutor js1 = (JavascriptExecutor) driver;
         js1.executeScript("arguments[0].click();", radio1);
 
-
+        Thread.sleep(ONE_AND_HALF);
         driver.findElement(By.xpath("//input[@wfd-id='id11']")).sendKeys("Иванов");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//input[@wfd-id='id12']")).sendKeys("Иван");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.xpath("//input[@wfd-id='id13']")).sendKeys("Платонович");
-        Thread.sleep(HALF_SECOND);
+        Thread.sleep(ONE_AND_HALF);
 
         driver.findElement(By.id("findFl")).click();
 
--- src/test/java/com/person/TimeSleep.java
+++ src/test/java/com/person/TimeSleep.java
@@ -3,6 +3,7 @@ package com.person;
 public class TimeSleep {
 
     public static int TWO_SECOND = 2000;
+    public static int ONE_AND_HALF = 1500;
     public static int FIVE_SECOND = 5000;
     public static int TEN_SECOND = 10000;
     public static int HALF_SECOND = 500;
